b97b41bf5a6fdd8521b30f0198a99cc30e8eaa42
TimmtyN <nguyentimot@gmail.com>
2026-03-04 19:08:34 -0500

    Add lag features, moving averages, and residual visualization for model evaluation

--- src/train_model.py
+++ src/train_model.py
@@ -10,11 +10,24 @@ df.columns = ["Date","Close","High","Low","Open","Volume"]
 
 df["Close"] = pd.to_numeric(df["Close"])
 
-df["Prev_Close"] = df["Close"].shift(1)
+df["Lag1"] = df["Close"].shift(1)
+df["Lag2"] = df["Close"].shift(2)
+df["Lag3"] = df["Close"].shift(3)
+df["Lag5"] = df["Close"].shift(5)
+df["MA5"] = df["Close"].rolling(5).mean()
+df["MA20"] = df["Close"].rolling(20).mean()
 
 df = df.dropna()
 
-X = df[["Prev_Close"]]
+X = df[
+    ["Lag1",
+    "Lag2",
+    "Lag3",
+    "Lag5",
+    "MA5",
+    "MA20"]
+    ]
+
 y = df["Close"]
 
 
@@ -36,8 +49,9 @@ model.fit(X_train, y_train)
 joblib.dump(model, "models/model.pkl")
 print("Model trained and saved")
 
+from sklearn.metrics import mean_squared_error
 
 preds = model.predict(X_test)
-rmse = np.sqrt(mean_absolute_error(y_test, preds))
+rmse = np.sqrt(mean_squared_error(y_test, preds))
 
 print("RMSE:", rmse)
